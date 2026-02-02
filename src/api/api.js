7456c4cd17cdd0b0531d444c99a803bf7ed0c2f6
Praveen Kumar Reddy Chinthapulusu <149413453+praveenmarch22@users.noreply.github.com>
2026-02-02 21:46:28 +0530

    added base url

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'http://13.201.133.172:5000/api';
 
 const api = axios.create({
   baseURL: API_URL,
@@ -53,3 +53,4 @@ export const addMoreWithdrawal = (id, amount, purpose = '') => {
 };
 
 export default api;
+
